e72bf43dfffd5b1fb65e881867e903950b5139cc
Isuru Fernando <isuruf-bot@users.noreply.github.com>
2019-01-28 20:52:53 +0000

    Format using clang-format-3.8

--- matchpygen/example_coroutine2.cpp
+++ matchpygen/example_coroutine2.cpp
@@ -26,7 +26,7 @@ int try_add_variable(Substitution2 &subst, string variable_name,
 
 typedef std::deque<RCP<const Basic>> Deque;
 
-Deque get_deque(RCP<const Basic>& expr)
+Deque get_deque(RCP<const Basic> &expr)
 {
     Deque d;
     for (RCP<const Basic> i : expr->get_args()) {
@@ -55,7 +55,7 @@ public:
     }
 
 protected:
-    void yield(int value_int_, Substitution2& value_subst_)
+    void yield(int value_int_, Substitution2 &value_subst_)
     {
         value_int = value_int_;
         value_subst = value_subst_;
@@ -74,7 +74,7 @@ public:
     RCP<const Basic> tmp1, tmp3, tmp4, tmp6;
     Substitution2 subst0, subst1, tmp_subst;
 
-    match_root(RCP<const Basic>& subject_) : subject(subject_)
+    match_root(RCP<const Basic> &subject_) : subject(subject_)
     {
         routine = coroutine::create(std::bind(&match_root::run, this));
     }
@@ -138,8 +138,8 @@ int main(int argc, char *argv[])
     auto result = mr1.next();
 
     RCP<const Basic> y = symbol("y");
-    std::cout << "Result for " << expr->__str__() << " is " << std::get<0>(result)
-              << std::endl;
+    std::cout << "Result for " << expr->__str__() << " is "
+              << std::get<0>(result) << std::endl;
     result = match_root(y).next();
     std::cout << "Result for " << y->__str__() << " is " << std::get<0>(result)
               << std::endl;
