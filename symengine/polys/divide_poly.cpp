e0c285d386af024b1add4481fa30f7994236b790
Isuru Fernando <isuruf-bot@users.noreply.github.com>
2019-11-09 14:37:46 +0000

    Format using clang-format-3.8

--- symengine/polys/divide_poly.cpp
+++ symengine/polys/divide_poly.cpp
@@ -7,10 +7,11 @@ using SymEngine::UIntPolyFlint;
 namespace SymEngine
 {
 
-void divide_poly(const RCP<const Basic> &dividend, const RCP<const Basic> &divisor,
-            const Ptr<RCP<const UIntPolyFlint>> &quotient,
-            const Ptr<RCP<const UIntPolyFlint>> &remainder)
+void divide_poly(const RCP<const Basic> &dividend,
+                 const RCP<const Basic> &divisor,
+                 const Ptr<RCP<const UIntPolyFlint>> &quotient,
+                 const Ptr<RCP<const UIntPolyFlint>> &remainder)
 {
-    //Implementation
+    // Implementation
 }
 }
