a06c510d67958233ac28af9cf7665cc5f96b9c37
Isuru Fernando <isuruf-bot@users.noreply.github.com>
2020-08-31 12:31:21 +0000

    Format using clang-format-3.8

--- benchmarks/bench_lambda_double.cpp
+++ benchmarks/bench_lambda_double.cpp
@@ -7,34 +7,34 @@
 using SymEngine::LambdaRealDoubleVisitor;
 
 #define SYMENGINE_LAMBDA_BENCH_CALL(NAME, EXPR, CSE)                           \
-  NONIUS_BENCHMARK(NAME, [](nonius::chronometer meter) {                       \
-    auto vec = get_vec();                                                      \
-    auto expr = get_expression_##EXPR(vec);                                    \
-    LambdaRealDoubleVisitor v;                                                 \
-    v.init(vec, expr, CSE);                                                    \
-    std::vector<double> s{0.0, 0.0, 0.0};                                      \
-    std::vector<double> d{0.0, 0.0, 0.0};                                      \
-    std::vector<double> x{1.0, 4.4365, 12.8};                                  \
-    meter.measure([&]() {                                                      \
-      for (std::size_t j = 0; j < call_iterations; ++j) {                      \
-        x[0] += 1.0;                                                           \
-        x[1] += 2.0;                                                           \
-        x[2] += 3.0;                                                           \
-        v.call(d.data(), x.data());                                            \
-        s[0] += d[0];                                                          \
-        s[1] += d[1];                                                          \
-        s[2] += d[2];                                                          \
-      }                                                                        \
-    });                                                                        \
-  })
+    NONIUS_BENCHMARK(NAME, [](nonius::chronometer meter) {                     \
+        auto vec = get_vec();                                                  \
+        auto expr = get_expression_##EXPR(vec);                                \
+        LambdaRealDoubleVisitor v;                                             \
+        v.init(vec, expr, CSE);                                                \
+        std::vector<double> s{0.0, 0.0, 0.0};                                  \
+        std::vector<double> d{0.0, 0.0, 0.0};                                  \
+        std::vector<double> x{1.0, 4.4365, 12.8};                              \
+        meter.measure([&]() {                                                  \
+            for (std::size_t j = 0; j < call_iterations; ++j) {                \
+                x[0] += 1.0;                                                   \
+                x[1] += 2.0;                                                   \
+                x[2] += 3.0;                                                   \
+                v.call(d.data(), x.data());                                    \
+                s[0] += d[0];                                                  \
+                s[1] += d[1];                                                  \
+                s[2] += d[2];                                                  \
+            }                                                                  \
+        });                                                                    \
+    })
 
 #define SYMENGINE_LAMBDA_BENCH_INIT(NAME, EXPR, CSE)                           \
-  NONIUS_BENCHMARK(NAME, [](nonius::chronometer meter) {                       \
-    auto vec = get_vec();                                                      \
-    auto expr = get_expression_##EXPR(vec);                                    \
-    LambdaRealDoubleVisitor v;                                                 \
-    meter.measure([&]() { v.init(vec, expr, CSE); });                          \
-  })
+    NONIUS_BENCHMARK(NAME, [](nonius::chronometer meter) {                     \
+        auto vec = get_vec();                                                  \
+        auto expr = get_expression_##EXPR(vec);                                \
+        LambdaRealDoubleVisitor v;                                             \
+        meter.measure([&]() { v.init(vec, expr, CSE); });                      \
+    })
 
 SYMENGINE_LAMBDA_BENCH_CALL("lambda_real_double_visitor_expr1_call", 1, false);
 SYMENGINE_LAMBDA_BENCH_CALL("lambda_real_double_visitor_expr1_cse_call", 1,
--- benchmarks/bench_llvm_double.cpp
+++ benchmarks/bench_llvm_double.cpp
@@ -7,34 +7,34 @@
 using SymEngine::LLVMDoubleVisitor;
 
 #define SYMENGINE_LLVM_BENCH_CALL(NAME, EXPR, CSE, OPT)                        \
-  NONIUS_BENCHMARK(NAME, [](nonius::chronometer meter) {                       \
-    auto vec = get_vec();                                                      \
-    auto expr = get_expression_##EXPR(vec);                                    \
-    LLVMDoubleVisitor v;                                                       \
-    v.init(vec, expr, CSE, OPT);                                               \
-    std::vector<double> s{0.0, 0.0, 0.0};                                      \
-    std::vector<double> d{0.0, 0.0, 0.0};                                      \
-    std::vector<double> x{1.0, 4.4365, 12.8};                                  \
-    meter.measure([&]() {                                                      \
-      for (std::size_t j = 0; j < call_iterations; ++j) {                      \
-        x[0] += 1.0;                                                           \
-        x[1] += 2.0;                                                           \
-        x[2] += 3.0;                                                           \
-        v.call(d.data(), x.data());                                            \
-        s[0] += d[0];                                                          \
-        s[1] += d[1];                                                          \
-        s[2] += d[2];                                                          \
-      }                                                                        \
-    });                                                                        \
-  })
+    NONIUS_BENCHMARK(NAME, [](nonius::chronometer meter) {                     \
+        auto vec = get_vec();                                                  \
+        auto expr = get_expression_##EXPR(vec);                                \
+        LLVMDoubleVisitor v;                                                   \
+        v.init(vec, expr, CSE, OPT);                                           \
+        std::vector<double> s{0.0, 0.0, 0.0};                                  \
+        std::vector<double> d{0.0, 0.0, 0.0};                                  \
+        std::vector<double> x{1.0, 4.4365, 12.8};                              \
+        meter.measure([&]() {                                                  \
+            for (std::size_t j = 0; j < call_iterations; ++j) {                \
+                x[0] += 1.0;                                                   \
+                x[1] += 2.0;                                                   \
+                x[2] += 3.0;                                                   \
+                v.call(d.data(), x.data());                                    \
+                s[0] += d[0];                                                  \
+                s[1] += d[1];                                                  \
+                s[2] += d[2];                                                  \
+            }                                                                  \
+        });                                                                    \
+    })
 
 #define SYMENGINE_LLVM_BENCH_INIT(NAME, EXPR, CSE, OPT)                        \
-  NONIUS_BENCHMARK(NAME, [](nonius::chronometer meter) {                       \
-    auto vec = get_vec();                                                      \
-    auto expr = get_expression_##EXPR(vec);                                    \
-    LLVMDoubleVisitor v;                                                       \
-    meter.measure([&]() { v.init(vec, expr, CSE, OPT); });                     \
-  })
+    NONIUS_BENCHMARK(NAME, [](nonius::chronometer meter) {                     \
+        auto vec = get_vec();                                                  \
+        auto expr = get_expression_##EXPR(vec);                                \
+        LLVMDoubleVisitor v;                                                   \
+        meter.measure([&]() { v.init(vec, expr, CSE, OPT); });                 \
+    })
 
 SYMENGINE_LLVM_BENCH_CALL("llvm_double_visitor_expr1_O0_call", 1, false, 0);
 SYMENGINE_LLVM_BENCH_CALL("llvm_double_visitor_expr1_O0_cse_call", 1, true, 0);
