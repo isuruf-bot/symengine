3d7e3434f27bcf36e9c38ac62492f70bcc0dedea
Isuru Fernando <isuruf-bot@users.noreply.github.com>
2020-09-02 14:01:52 +0000

    Format using clang-format-3.8

--- benchmarks/visitor_call.cpp
+++ benchmarks/visitor_call.cpp
@@ -23,111 +23,127 @@ using SymEngine::symbol;
 using SymEngine::vec_basic;
 
 struct Expr1 {
-  vec_basic vec{{symbol("x"), symbol("y"), symbol("z")}};
-  vec_basic expr() {
-    vec_basic r{symbol("r0")};
-    r[0] = sin(
-        add(vec[0], cos(add(mul(vec[1], vec[2]), pow(vec[0], integer(2))))));
-    r[0] = mul(add(integer(3), r[0]), add(integer(2), r[0]));
-    r[0] = pow(add(integer(5), r[0]), add(integer(-2), r[0]));
-    return r;
-  }
-  static void compiled_expr(double *d, const double *v) {
-    double r1 = std::sin(v[0] + std::cos((v[1] * v[2]) + std::pow(v[0], 2)));
-    double r2 = (3 + r1) * (2 + r1);
-    *d = std::pow((5 + r2), (r2 - 2));
-  }
-  static void compiled_expr(float *d, const float *v) {
-    float r1 = sinf(v[0] + cosf((v[1] * v[2]) + powf(v[0], 2)));
-    float r2 = (3 + r1) * (2 + r1);
-    *d = powf((5 + r2), (r2 - 2));
-  }
+    vec_basic vec{{symbol("x"), symbol("y"), symbol("z")}};
+    vec_basic expr()
+    {
+        vec_basic r{symbol("r0")};
+        r[0] = sin(add(vec[0],
+                       cos(add(mul(vec[1], vec[2]), pow(vec[0], integer(2))))));
+        r[0] = mul(add(integer(3), r[0]), add(integer(2), r[0]));
+        r[0] = pow(add(integer(5), r[0]), add(integer(-2), r[0]));
+        return r;
+    }
+    static void compiled_expr(double *d, const double *v)
+    {
+        double r1
+            = std::sin(v[0] + std::cos((v[1] * v[2]) + std::pow(v[0], 2)));
+        double r2 = (3 + r1) * (2 + r1);
+        *d = std::pow((5 + r2), (r2 - 2));
+    }
+    static void compiled_expr(float *d, const float *v)
+    {
+        float r1 = sinf(v[0] + cosf((v[1] * v[2]) + powf(v[0], 2)));
+        float r2 = (3 + r1) * (2 + r1);
+        *d = powf((5 + r2), (r2 - 2));
+    }
 };
 
 struct Expr2 {
-  vec_basic vec{{symbol("x"), symbol("y"), symbol("z")}};
-  vec_basic expr() {
-    vec_basic r{symbol("r0"), symbol("r1"), symbol("r2")};
-    r[0] = mul(integer(2), add(vec[0], add(vec[0], mul(vec[1], vec[2]))));
-    r[1] = add(vec[0], add(vec[0], mul(vec[2], vec[1])));
-    r[2] = mul(integer(-2), add(vec[0], add(vec[0], mul(vec[1], vec[2]))));
-    return r;
-  }
-  template <typename Real> static void compiled_expr(Real *d, const Real *v) {
-    d[0] = 2.0 * (v[0] + v[0] + (v[1] * v[2]));
-    d[1] = v[0] + v[0] + (v[2] * v[1]);
-    d[2] = -2.0 * (v[0] + v[0] + (v[1] * v[2]));
-  }
+    vec_basic vec{{symbol("x"), symbol("y"), symbol("z")}};
+    vec_basic expr()
+    {
+        vec_basic r{symbol("r0"), symbol("r1"), symbol("r2")};
+        r[0] = mul(integer(2), add(vec[0], add(vec[0], mul(vec[1], vec[2]))));
+        r[1] = add(vec[0], add(vec[0], mul(vec[2], vec[1])));
+        r[2] = mul(integer(-2), add(vec[0], add(vec[0], mul(vec[1], vec[2]))));
+        return r;
+    }
+    template <typename Real>
+    static void compiled_expr(Real *d, const Real *v)
+    {
+        d[0] = 2.0 * (v[0] + v[0] + (v[1] * v[2]));
+        d[1] = v[0] + v[0] + (v[2] * v[1]);
+        d[2] = -2.0 * (v[0] + v[0] + (v[1] * v[2]));
+    }
 };
 
-template <typename Real> struct NativeVisitor {
-  void (*f)(Real *, const Real *){nullptr};
-  void call(Real *d, const Real *v) { f(d, v); }
+template <typename Real>
+struct NativeVisitor {
+    void (*f)(Real *, const Real *){nullptr};
+    void call(Real *d, const Real *v)
+    {
+        f(d, v);
+    }
 };
 
 template <typename Expr, typename Real>
-std::string init(NativeVisitor<Real> &v, bool cse, unsigned opt_level) {
-  v.f = &Expr::compiled_expr;
-  return {};
+std::string init(NativeVisitor<Real> &v, bool cse, unsigned opt_level)
+{
+    v.f = &Expr::compiled_expr;
+    return {};
 }
 
 template <typename Expr>
-std::string init(LambdaRealDoubleVisitor &v, bool cse, unsigned opt_level) {
-  std::string label;
-  Expr e;
-  v.init(e.vec, e.expr(), cse);
-  if (cse) {
-    label = "cse";
-  }
-  return label;
+std::string init(LambdaRealDoubleVisitor &v, bool cse, unsigned opt_level)
+{
+    std::string label;
+    Expr e;
+    v.init(e.vec, e.expr(), cse);
+    if (cse) {
+        label = "cse";
+    }
+    return label;
 }
 
 template <typename Expr>
-std::string init(LLVMDoubleVisitor &v, bool cse, unsigned opt_level) {
-  std::string label;
-  Expr e;
-  v.init(e.vec, e.expr(), cse, opt_level);
-  if (cse) {
-    label.append("cse_");
-  }
-  label.append("O");
-  label.append(std::to_string(opt_level));
-  return label;
+std::string init(LLVMDoubleVisitor &v, bool cse, unsigned opt_level)
+{
+    std::string label;
+    Expr e;
+    v.init(e.vec, e.expr(), cse, opt_level);
+    if (cse) {
+        label.append("cse_");
+    }
+    label.append("O");
+    label.append(std::to_string(opt_level));
+    return label;
 }
 
 template <typename Expr>
-std::string init(LLVMFloatVisitor &v, bool cse, unsigned opt_level) {
-  std::string label;
-  Expr e;
-  v.init(e.vec, e.expr(), cse, opt_level);
-  if (cse) {
-    label.append("cse_");
-  }
-  label.append("O");
-  label.append(std::to_string(opt_level));
-  return label;
+std::string init(LLVMFloatVisitor &v, bool cse, unsigned opt_level)
+{
+    std::string label;
+    Expr e;
+    v.init(e.vec, e.expr(), cse, opt_level);
+    if (cse) {
+        label.append("cse_");
+    }
+    label.append("O");
+    label.append(std::to_string(opt_level));
+    return label;
 }
 
 template <typename Visitor, typename Expr, typename Real>
-static void Call(benchmark::State &state) {
-  std::vector<Real> s{0.0, 0.0, 0.0};
-  std::vector<Real> d{0.0, 0.0, 0.0};
-  std::vector<Real> x{1.0, 4.4365, 12.8};
-  Visitor v;
-  bool cse{static_cast<bool>(state.range(0))};
-  unsigned opt_level{static_cast<unsigned>(state.range(1))};
-  auto label = init<Expr>(v, cse, opt_level);
-  for (auto _ : state) {
-    x[0] += 0.1;
-    x[1] += 0.2;
-    x[2] += 0.3;
-    v.call(d.data(), x.data());
-    benchmark::ClobberMemory();
-    s[0] += d[0];
-    s[1] += d[1];
-    s[2] += d[2];
-  }
-  state.SetLabel(label);
+static void Call(benchmark::State &state)
+{
+    std::vector<Real> s{0.0, 0.0, 0.0};
+    std::vector<Real> d{0.0, 0.0, 0.0};
+    std::vector<Real> x{1.0, 4.4365, 12.8};
+    Visitor v;
+    bool cse{static_cast<bool>(state.range(0))};
+    unsigned opt_level{static_cast<unsigned>(state.range(1))};
+    auto label = init<Expr>(v, cse, opt_level);
+    for (auto _ : state) {
+        x[0] += 0.1;
+        x[1] += 0.2;
+        x[2] += 0.3;
+        v.call(d.data(), x.data());
+        benchmark::ClobberMemory();
+        s[0] += d[0];
+        s[1] += d[1];
+        s[2] += d[2];
+    }
+    state.SetLabel(label);
 }
 
 // BENCHMARK_TEMPLATE(BenchmarkName, VisitorClass,
